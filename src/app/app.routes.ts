8d0cf484a3a148f43140ed30bd36460811b476e3
Warodom Techafong <andrewafnz@gmail.com>
2023-07-24 17:03:06 +0700

    week5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
+import { TryThreeComponent } from './try-three/try-three.component';
 import { Routes } from '@angular/router';
+import { TryOneComponent } from './try-one/try-one.component';
+import { TryTwoComponent } from './try-two/try-two.component';
+import { TryFourComponent } from './lab-week5/lab-week5.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'try-one', component: TryOneComponent },
+  { path: 'try-two', component: TryTwoComponent },
+  { path: 'try-three', component: TryThreeComponent },
+  { path: 'lab-week5', component: TryFourComponent },
+];
